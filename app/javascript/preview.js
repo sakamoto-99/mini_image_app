21875b640c6d4d74714ad9f107ba37f4db0f80c1
sakamoto-99 <sakamoto.engineer.99@gmail.com>
2021-06-18 17:43:07 +0900

    複数枚画像投稿の実装2

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -4,8 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // 選択した画像を表示する関数
     const createImageHTML = (blob) => {
-       // 画像を表示するためのdiv要素を生成
+      // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div')
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
 
       // 表示する画像を生成
       const blobImage = document.createElement('img')
@@ -13,17 +15,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
       // ファイル選択ボタンを生成
       const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `message_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'message[images][]')
+      inputHTML.setAttribute('type', 'file')
 
       // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage)
+      imageElement.appendChild(inputHTML)
       ImageList.appendChild(imageElement)
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      })
     }
 
     document.getElementById('message_image').addEventListener('change', (e) => {
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
 
       createImageHTML(blob)
+
     });
   }
 });
